048ddfccb00dab8f9c2900032adab8e2cf9478f7
8ctopotamus <joshnaylor88@gmail.com>
2019-01-24 12:48:21 -0600

    Added React Context to AppBar

--- src/app/index.js
+++ src/app/index.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import AppLayout from './AppLayout'
 import AppBar from './AppBar'
-// import styled, { css } from 'styled-components'
+import { AppProvider } from './AppProvider'
 
 class App extends Component {
   render() {
     return (
       <div>
         <AppLayout>
-          <AppBar />
-          Welcome to CryptoDash
+          <AppProvider>
+            <AppBar />
+            <p>Welcome to CryptoDash</p>
+          </AppProvider>
         </AppLayout>
       </div>
     );

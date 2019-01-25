d22c09a5a07640e0d1dbe5965b9faa70f51a5729
8ctopotamus <joshnaylor88@gmail.com>
2019-01-24 18:23:20 -0600

    Responsive Grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -5,7 +5,7 @@ import CoinTile from './CoinTile'
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
   grid-gap: 15px;
   margin-top: 40px;
 `
@@ -15,12 +15,12 @@ function getCoinsToDisplay(coinList, topSection, favorites) {
 }
 
 export default function({ topSection }) {
-  return <AppContext>
+  return <AppContext.Consumer>
     {({ coinList, favorites }) => <CoinGridStyled>
         {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey =>
-          <CoinTile topSection={topSection} coinKey={coinKey} />
+          <CoinTile topSection={topSection} coinKey={coinKey} key={coinKey} />
         )}
       </CoinGridStyled>
     }
-  </AppContext>
+  </AppContext.Consumer>
 }

a4cb49b56271744c964c38ca1d543bf67b7f4441
8ctopotamus <joshnaylor88@gmail.com>
2019-01-25 12:23:18 -0600

    Updates

---
+++ precache-manifest.e5177b55c9e8f133e48c0e5372f84b1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24ddf761258e27c1e679",
+    "url": "/cryptodash/static/css/main.6cc5916e.chunk.css"
+  },
+  {
+    "revision": "24ddf761258e27c1e679",
+    "url": "/cryptodash/static/js/main.24ddf761.chunk.js"
+  },
+  {
+    "revision": "866f580552221ef6c899",
+    "url": "/cryptodash/static/js/1.866f5805.chunk.js"
+  },
+  {
+    "revision": "58b9b0f81cc496005cd3",
+    "url": "/cryptodash/static/js/runtime~main.58b9b0f8.js"
+  },
+  {
+    "revision": "2a6ec6ae13f2433eb80bb38d2c812fe8",
+    "url": "/cryptodash/index.html"
+  }
+];
